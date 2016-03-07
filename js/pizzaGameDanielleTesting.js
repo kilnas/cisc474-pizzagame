bc571fa4808446dcb7d32d9fe6fc991aae610ca6
kalina <kalinaye@gmail.com>
2016-03-07 00:25:16 +0000

    mar6 723pm

--- js/pizzaGameDanielleTesting.js
+++ js/pizzaGameDanielleTesting.js
@@ -21,7 +21,6 @@ var PizzaGame = function(level, pizzaPlayer, door){ // game object
     this.listOfEnemies = {};
     this.listOfIngredients = {};
     this.listOfToppings = {};
-    this.listOfPlatforms = {};
     this.pizzaPlayer = pizzaPlayer;
     this.door = door;
     this.width = 900;
@@ -35,9 +34,9 @@ var PizzaGame = function(level, pizzaPlayer, door){ // game object
     
     //check if player has collected all ingredients
     this.checkIngredientCompletion = function(){
+        console.log("checkin " + self.listOfIngredients);
         var complete = true;
-        for(var i=0; i < self.listOfIngredients.length; i++){
-            console.log("checkin " + self.listOfIngredients[i].type);
+        for(var i=0; i < self.listOfIngredients.length, complete; i++){
             complete = complete && self.listOfIngredients[i].isCollected;
         }
         
@@ -46,7 +45,7 @@ var PizzaGame = function(level, pizzaPlayer, door){ // game object
             self.ingredientsComplete = true;
             self.door.isUnlocked = true;
             console.log("You collected all the ingredients! Now you can get toppings and be eaten.");
-            alert("You collected all the ingredients! Now you can get toppings and be eaten.");
+            alet("You collected all the ingredients! Now you can get toppings and be eaten.");
         }
     };
 }
@@ -62,10 +61,6 @@ var PizzaPlayer = function(xPos, yPos, width, height){ // main character player
     this.isJumping = false;
     this.toppings = [];
     this.currToppingsLength = 0; //counter to check for change
-    this.isHittingDoor = false;
-    this.isHittingEnemy=false;
-    this.isHittingTopping = false;
-    this.isHittingIngredient = false;
 
     this.moveLeft = function(value){
         self.xPos = self.xPos - value;
@@ -79,10 +74,15 @@ var PizzaPlayer = function(xPos, yPos, width, height){ // main character player
     
     this.jump = function(){
         if (self.isJumping == false) {
-            self.yVel = -60;//-60
+            self.yVel = -60;
         self.isJumping = true;
-        }
+    }
+        //self.yPos = self.yPos - 100;// FIGURE OUT PHYSICS
      
+    }
+    
+    this.updatePosition = function(){ //????????????
+        
     }
     
     //this.initialize();
@@ -95,13 +95,7 @@ var PizzaPlayer = function(xPos, yPos, width, height){ // main character player
     
 }
 
-//a platform in the game map
-var Platform = function(xPos, yPos, width, height){
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.width = width;
-    this.height = height;
-}
+
 
 
 //The door for completing the game. Unlocks after you collect the basic ingredients.
@@ -148,43 +142,33 @@ var colliding = function(player, object){
 //check this item off the game's list and update score/lives/door/etc.
 var collectItem = function(game, itemIndex, array){
     var item = array[itemIndex];
+    console.log("collecting item: " + item.type());
     //if ingredient, check if we can unlock the door now
     if(item.isRequired){
         item.isCollected = true;
         game.score += item.points;
         game.checkIngredientCompletion(); 
-        console.log("collected " + item.type + " at " + item.xPos + ", " + item.yPos);
         console.log("score: " + game.score);
     }
     //if topping, and allowed to collect toppings, increase score
     else if(game.ingredientsComplete){
         item.isCollected = true;
         game.score += item.points;
-        console.log("collected " + item.type + " at " + item.xPos + ", " + item.yPos);
         console.log("score: " + game.score);
     } 
     //else, you tried to collect topping when not allowed yet
     else{
         console.log("You can't collect toppings until you have the basic ingredients!");
-        alert("You can't collect toppings until you have the basic ingredients!");
     }
 };
 
-//return true if player is hitting anything in array
-var isHittingThingInArray = function(player, list){
-  var hit = false;
-   for(var i=0; i < list.length && !hit; i++){
-       hit = hit && colliding(player, list[i]);
-   }
-}
 
 //check if player collided with any uncollected objects/enemies & react accordingly
 var checkCollisions = function(game){
     
     //check ingredients
     for(var i=0; i < game.listOfIngredients.length; i++){
-        if(colliding(game.pizzaPlayer, game.listOfIngredients[i]) 
-        && !game.listOfIngredients[i].isCollected && !game.pizzaPlayer.isHittingIngredient){
+        if(colliding(game.pizzaPlayer, game.listOfIngredients[i]) && !game.listOfIngredients[i].isCollected){
             //collect
             collectItem(game, i, game.listOfIngredients);
         }
@@ -192,34 +176,18 @@ var checkCollisions = function(game){
     
     //check toppings
     for(var i=0; i < game.listOfToppings.length; i++){
-        if(colliding(game.pizzaPlayer, game.listOfToppings[i]) && !game.listOfToppings[i].isCollected
-        && !game.pizzaPlayer.isHittingTopping){
+        if(colliding(game.pizzaPlayer, game.listOfToppings[i]) && !game.listOfToppings[i].isCollected){
            //collect
             collectItem(game, i, game.listOfToppings);
-        }
-        else if(!isHittingThingInArray(game.pizzaPlayer, game.listOfToppings[i])){
-            game.pizzaPlayer.isHittingTopping = false;
-    }
+        }    
     }
     
     //check enemies
     for(var i=0; i < game.listOfEnemies.length; i++){
-        if(colliding(game.pizzaPlayer, game.listOfEnemies[i])&& !game.pizzaPlayer.isHittingEnemy){
+        if(colliding(game.pizzaPlayer, game.listOfEnemies[i])){
           //make lives go down or w/e depending on enemy
          PizzaGame.lives -= 1;
-         game.pizzaPlayer.isHittingEnemy=true;
-         console.log("you got eaten a bit!")
-        } else if(!colliding(game.pizzaPlayer, game.listOfEnemies[i])){
-            game.pizzaPlayer.isHittingEnemy=false;
-        }
-    }
-    
-    //check door
-    if(colliding(game.pizzaPlayer, game.door) && !game.pizzaPlayer.isHittingDoor){
-        console.log("HIT DOOR");
-        game.pizzaPlayer.isHittingDoor = true;
-    }else if(!colliding(game.pizzaPlayer, game.door)){
-        game.pizzaPlayer.isHittingDoor = false;
+        }    
     }
 }
 
@@ -234,12 +202,24 @@ var Enemy = function(xPos, yPos, type, speed, width, height,minx,maxx){
     this.height = height;
     this.minx=minx;
     this.maxx=maxx;
-  
+    this.initialize=function(){
+        
+    };
     this.setPosition=function(x,y){
         self.xPos=x;
         self.yPos=y;
     };
-    
+    this.update=function(){
+        
+    }
+  // this.updatePos=function(time){
+       //var distance= speed*time;
+      // if(xPos<self.minx){
+      //     self.xPos=self.xPos+distance;
+      // }
+      // else if(xPos>self.maxx){
+      //     self.xPos=self.xPos-distance;
+      // }
       function updateEnemy(){
           if (xPos<self.minx){
               self.xPos+=1;
@@ -250,7 +230,8 @@ var Enemy = function(xPos, yPos, type, speed, width, height,minx,maxx){
               self.update();
           }
       } 
-    }
+    this.initialize();
+   }
    
 
 
@@ -296,18 +277,15 @@ var drawEnemy = function(Enemy){
     $Enemy.style.left=Enemy.xPos+"px";
     $Enemy.style.top= Enemy.yPos+"px";
     $Enemy.style.width=Enemy.width+"px";
-    $Enemy.style.height=Enemy.height+"px";
+    $Enemy.style.height=Enemy.height+"px";Enemy
     $Enemy.style.backgroundColor = "red";
-    $Enemy.classList.add("Enemy");
     $("#gameBoard").append($Enemy);
-   
   /* for(var i=0; i < game.listOfEnemies.length; i++){
     drawEnemy(game.listOfEnemies[i]);
     }*/
 }
 var drawEnemies = function(game){
     $(".Enemy").remove();
-    
     for(var i=0; i<game.listOfEnemies.length; i++){
         drawEnemy(game.listOfEnemies[i]);
     }
@@ -362,10 +340,7 @@ var drawCollectables = function(game){
     }
 }
 
-// global flag for lives check
-var livesFlag = true;
-
-var drawLives = function(game) {
+var drawLives = function() {
     var initialize = function(){
         //life 1
         var circle1 = document.createElement('ul');
@@ -373,7 +348,7 @@ var drawLives = function(game) {
         var slice1 = document.createElement('li');
         slice1.setAttribute('id','slice');
         var slice1contents = document.createElement('div');
-        slice1contents.setAttribute('id','slice-contents');
+        slice1contents.setAttribute('id','slice1-contents');
         slice1.appendChild(slice1contents);
         circle1.appendChild(slice1);
         
@@ -383,7 +358,7 @@ var drawLives = function(game) {
         var slice2 = document.createElement('li');
         slice2.setAttribute('id','slice');
         var slice2contents = document.createElement('div');
-        slice2contents.setAttribute('id','slice-contents');
+        slice2contents.setAttribute('id','slice2-contents');
         slice2.appendChild(slice2contents);
         circle2.appendChild(slice2);
         
@@ -393,7 +368,7 @@ var drawLives = function(game) {
         var slice3 = document.createElement('li');
         slice3.setAttribute('id','slice');
         var slice3contents = document.createElement('div');
-        slice3contents.setAttribute('id','slice-contents');
+        slice3contents.setAttribute('id','slice3-contents');
         slice3.appendChild(slice3contents);
         circle3.appendChild(slice3);
         
@@ -402,59 +377,16 @@ var drawLives = function(game) {
         lives.appendChild(circle2);
         lives.appendChild(circle3);
     }
-    if (livesFlag){
-        initialize();
-        livesFlag = false;
-    }
-    if (game.lives == 2){
-        var lives = document.getElementById('lives');
-        var circle3 = document.getElementById('circle3');
-        lives.removeChild(circle3);
-    }
-    if (game.lives == 1){
-        var lives = document.getElementById('lives');
-        var circle2 = document.getElementById('circle2');
-        lives.removeChild(circle2);
-    }
-    if (game.lives == 0){
-        var lives = document.getElementById('lives');
-        var circle1 = document.getElementById('circle1');
-        lives.removeChild(circle1);
-    }
 }
 
 var drawDoor = function(door){
     var $door = document.createElement("div");
-    $door.setAttribute('id', 'mouthDoor');
-    $door.style.left = door.xPos + 'px';
-    $door.style.top = door.yPos + 'px';
-    $door.style.width = door.width + 'px';
-    $door.style.height = door.height + 'px';
+    $door.setAttribute('id', '#mouthDoor');
+    $door.style.left('left', door.xPos + 'px');
     //$door.css('left', xPos + "px");
     
     $('#gameBoard').append($door);
     
-}
-
-var drawPlatform = function(platform){
-    var $item = document.createElement("div");
-    $item.classList.add("platform");
-    $item.style.left = platform.xPos+"px";
-    $item.style.top = platform.yPos+"px";
-    $item.style.width = platform.width + "px",
-    $item.style.height = platform.height + "px",
-    
-    $('#gameBoard').append($item);
-}
-
-var drawPlatforms = function(game){
-    //clear all collectables from screen
-    $(".platform").remove();
-    
-    //redraw
-    for(var i=0; i < game.listOfPlatforms.length; i++){
-        drawCollectable(game.listOfPlatforms[i]);
-    }
 }
 
 //-------------------------------------- CONTROLLER stuff
@@ -464,7 +396,7 @@ var drawPlatforms = function(game){
 
 // player key press
 var movePlayer = function(pizzaPlayer){
-    //console.log(pizzaPlayer);
+    console.log(pizzaPlayer);
     $("body").keydown(function(e){
         var code = e.which; //e.keyCode || 
         
@@ -494,7 +426,7 @@ var movePlayer = function(pizzaPlayer){
             pizzaPlayer.jump();
          
             //drawPizzaPlayer(pizzaPlayer);
-            
+            console.log(pizzaPlayer);
             
         }else{
             // do nothing if other keys
@@ -510,7 +442,6 @@ var movePlayer = function(pizzaPlayer){
 var startGame = function(game, player){
   //testCollide();
     if(game.isRunning){
-        drawDoor(testDoor);
         if(player.xPos >= GROUNDLINE){
             $("#pizzaPlayer").css({
                   "-webkit-transition": "all 0.3s ease-out",
@@ -528,7 +459,6 @@ var startGame = function(game, player){
 
 $("#startButton").click(function(){
     testGame.isRunning = true;
-    livesFlag = true;
     startGame(testGame, pizza1);
     
 });
@@ -536,17 +466,15 @@ $("#startButton").click(function(){
 //----------------------------------------- game stuff
 
 var pizza1 = new PizzaPlayer(20, 400, 50, 50);
-var testDoor = new Door(500, 200, 100, 100);
+var testDoor = new Door(100, 100, 300, 200);
 var testGame = new PizzaGame(1, pizza1, testDoor);
-
-
 console.log(testGame.isRunning);
 
 
 //testenemy
 var testEnemy = [
-    
-   new Enemy(200,400,"Squirrel",1,20,20,-5,10)
+    new Enemy(50, 100, "Squirrel", 1, 20, 20, 0,10),
+    new Enemy(90, 100, "Bird", 2, 20, 20, 0, 10)
     ];
 
 var testIngredients = [
@@ -560,32 +488,23 @@ var testToppings = [
     new Collectable("pepperoni", 50, false, 600, 300, 20, 20)
 ];
 
-var testPlatforms = [
-    new Platform(300, 300, 50, 50)
-];
-
 testGame.listOfIngredients = testIngredients;
-testGame.listOfToppings = testToppings;
-testGame.listOfPlatforms = testPlatforms;
+//testGame.listOfToppings = testToppings;
 
-testGame.listOfEnemies=testEnemy;
 
-//test update - checks for collisions and collectable updates every 70ms
+
+//test update - checks for collisions and collectable updates every 200ms
 var testUpdate = function(){
     setTimeout(function(){
-        drawPizzaPlayer(pizza1); //note: draw before collision checks!
-        
         checkCollisions(testGame);
-        drawPlatforms(testGame);
         drawCollectables(testGame);
-        drawEnemies(testGame);
-        drawLives(testGame);
         
+        drawPizzaPlayer(pizza1);
+        drawDoor(800, 500);
         
          if (pizza1.isJumping) {
             pizza1.yVel += GRAVITY;
             pizza1.yPos += pizza1.yVel;
-            //console.log("YLOC: " + pizza1.yPos);
          if (pizza1.yPos > GROUNDLINE) {
              pizza1.yPos = GROUNDLINE;
              pizza1.yVel = 0;
@@ -652,6 +571,8 @@ var testCollide = function(){
     
     
 $(document).ready(function(){
+
+    
     
 });
 
